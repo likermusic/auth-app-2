660b502a0f45f86b38b4c8d33c0a41c748dbe4a8
Sergey Timofeev <drumliker@yandex.ru>
2025-05-18 12:15:57 +0300

    init libs

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Test } from "@/entities/Test";
+
+export function App() {
+  return (
+    <Button size={"lg"} className="text-black-800 ">
+      BButton
+    </Button>
+  );
+}
