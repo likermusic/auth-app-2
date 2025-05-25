b764bc1af837c58711e80098b6f4b5e495c2fcc7
Sergey Timofeev <drumliker@yandex.ru>
2025-05-25 12:15:33 +0300

    add User api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
-import { api } from "@/shared/api/axios-instance";
+import { authApi } from "@/entities/user/api/auth";
 import { Button } from "@/shared/ui/button";
 
 export function App() {
-  api.get("/").then((resp) => console.log(resp));
+  // api.get("/").then((resp) => console.log(resp));
+  authApi
+    .signin({ email: "test@test.ru", password: "1234" })
+    .then((resp) => console.log(resp.data));
   return (
     <Button size={"lg"} className="text-black-800 ">
       BButton
