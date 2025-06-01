e1ff3a143e42377ced32b1ac74d45bd09cc560ff
Sergey Timofeev <drumliker@yandex.ru>
2025-06-01 11:19:42 +0300

    add app router

---
+++ src/pages/signin/ui/Signin.tsx
@@ -0,0 +1,18 @@
+import { authApi } from "@/entities/user/api/auth";
+import { Button } from "@/shared/ui/button";
+import { AxiosError } from "axios";
+
+export const Signin = () => {
+  authApi
+    .signin({ email: "admin@mail.ru", password: "1234" })
+    .then((resp) => console.log(resp.data.message))
+    .catch((error: AxiosError<{ error: string }>) => {
+      console.log(error.response?.data.error);
+    });
+  return (
+    <h1>Signin page</h1>
+    // <Button size={"lg"} className="text-black-800 ">
+    //   Button
+    // </Button>
+  );
+};
