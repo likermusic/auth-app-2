c934b1c44211fd4eb10c44f63e3eef1c049b4971
Sergey Timofeev <drumliker@yandex.ru>
2025-06-01 10:48:25 +0300

    add auth methods to server

--- server/server.js
+++ server/server.js
@@ -15,9 +15,46 @@ app.get("/", (req, resp) => {
 });
 
 app.post("/signin", (req, resp) => {
-  console.log(req.body);
+  if (!req.body.email || !req.body.password) {
+    return resp
+      .status(400)
+      .json({ error: "Вы должны передать email и password" });
+  }
+  const { email, password } = req.body;
+  const users = [
+    { email: "admin@mail.ru", password: "1234" },
+    { email: "user@mail.ru", password: "1234" },
+  ];
+  const user = users.find(
+    (user) => user.email === email && user.password === password,
+  );
+  if (user) {
+    resp.status(200).json({ message: "Вы успешно авторизованы" });
+  } else {
+    resp.status(401).json({ error: "Пользователь не найден" });
+  }
+});
 
-  resp.json("Signin success");
+app.post("/signup", (req, resp) => {
+  if (!req.body.email || !req.body.password) {
+    return resp
+      .status(400)
+      .json({ error: "Вы должны передать email и password" });
+  }
+  const { email, password } = req.body;
+  const users = [
+    { email: "admin@mail.ru", password: "1234" },
+    { email: "user@mail.ru", password: "1234" },
+  ];
+  const user = users.some(
+    (user) => user.email === email && user.password === password,
+  );
+  if (user) {
+    resp.status(400).json({ error: "Пользователь уже существует" });
+  } else {
+    users.push({ email, password });
+    resp.status(201).json({ message: "Вы успешно зарегистрировались" });
+  }
 });
 
 app.listen(4000, () => console.log("Server started"));
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,18 @@
 import { authApi } from "@/entities/user/api/auth";
 import { Button } from "@/shared/ui/button";
+import { AxiosError } from "axios";
 
 export function App() {
   // api.get("/").then((resp) => console.log(resp));
   authApi
-    .signin({ email: "test@test.ru", password: "1234" })
-    .then((resp) => console.log(resp.data));
+    .signin({ email: "admin@mail.ru", password: "1234" })
+    .then((resp) => console.log(resp.data.message))
+    .catch((error: AxiosError<{ error: string }>) => {
+      console.log(error.response?.data.error);
+    });
   return (
     <Button size={"lg"} className="text-black-800 ">
-      BButton
+      Button
     </Button>
   );
 }
