2e5e8f93fa1501e10cb4971278d7c3197b082457
Sergey Timofeev <drumliker@yandex.ru>
2025-06-01 11:19:42 +0300

    add app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,5 @@
-import { authApi } from "@/entities/user/api/auth";
-import { Button } from "@/shared/ui/button";
-import { AxiosError } from "axios";
+import { AppRouter } from "./AppRouter";
 
 export function App() {
-  // api.get("/").then((resp) => console.log(resp));
-  authApi
-    .signin({ email: "admin@mail.ru", password: "1234" })
-    .then((resp) => console.log(resp.data.message))
-    .catch((error: AxiosError<{ error: string }>) => {
-      console.log(error.response?.data.error);
-    });
-  return (
-    <Button size={"lg"} className="text-black-800 ">
-      Button
-    </Button>
-  );
+  return <AppRouter />;
 }
