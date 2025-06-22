6275fdad59d0d31c64297297676c5c38b7612284
Sergey Timofeev <drumliker@yandex.ru>
2025-06-22 16:23:58 +0300

    add toaster when fail

--- src/features/auth/model/useSignin.ts
+++ src/features/auth/model/useSignin.ts
@@ -1,13 +1,26 @@
+import { authApi } from "@/entities/user";
 import { SigninFormSchema } from "./formSchema";
-import { authApi } from "@/entities/user/api/auth";
 import type { AxiosError } from "axios";
 import type { z } from "zod";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "@/shared/router/constants";
+import { toast } from "sonner";
 
 export const useSignin = () => {
+  const navigate = useNavigate();
   const signinHandler = async (data: z.infer<typeof SigninFormSchema>) => {
     console.log("signinHandler");
+
+    try {
+      throw new Error();
+      await authApi.signin(data);
+      navigate(ROUTES.HOME);
+    } catch (error) {
+      toast.error("Signin fail");
+    }
+
     // authApi
-    //   .signup({ email: "admin@mail.ru", password: "1234" })
+    //   .signin({ email: "admin@mail.ru", password: "1234" })
     //   .then((resp) => console.log(resp.data.message))
     //   .catch((error: AxiosError<{ error: string }>) => {
     //     console.log(error.response?.data.error);
