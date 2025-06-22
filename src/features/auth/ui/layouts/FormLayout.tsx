a1d24c7c3a4a5f1c7671eb210871ae99d4eb500b
Sergey Timofeev <drumliker@yandex.ru>
2025-06-22 16:03:22 +0300

    fix form schema & zod

--- src/features/auth/ui/layouts/FormLayout.tsx
+++ src/features/auth/ui/layouts/FormLayout.tsx
@@ -2,86 +2,52 @@ import { Button } from "@/shared/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/shared/ui/form";
 import { Input } from "@/shared/ui/input";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Eye, EyeOff } from "lucide-react";
 
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "react-router-dom";
-
-const emailMin = 6;
-const passwordMin = 4;
-const passwordMax = 20;
-
-const FormSchema = z
-  .object({
-    email: z
-      .string()
-      .email()
-      .min(emailMin, `Email must be at least ${emailMin} characters.`),
-    password: z
-      .string()
-      .min(
-        passwordMin,
-        `Password must not be less than ${passwordMin} characters.`,
-      )
-      .max(
-        passwordMax,
-        `Password must not be more than ${passwordMax} characters.`,
-      )
-      .regex(/[A-Z]/, "Password must contain capital characters.")
-      .regex(/[a-z]/, "Password must contain small characters.")
-      .regex(/[0-9]/, "Password must contain numeric characters."),
-
-    confirmPassword: z
-      .string()
-      .min(
-        passwordMin,
-        `Password must not be less than ${passwordMin} characters.`,
-      )
-      .max(
-        passwordMax,
-        `Password must not be more than ${passwordMax} characters.`,
-      )
-      .regex(/[A-Z]/, "Password must contain capital characters.")
-      .regex(/[a-z]/, "Password must contain small characters.")
-      .regex(/[0-9]/, "Password must contain numeric characters.")
-      .optional(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["confirmPassword"],
-  });
+import type {
+  SigninFormSchema,
+  SignupFormSchema,
+} from "../../model/formSchema";
 
 interface FormLayoutProps {
-  title: string;
-  onSubmit: (data: FormData) => void;
+  buttonTitle: string;
+  onSubmit: (
+    data: z.infer<typeof SigninFormSchema> | z.infer<typeof SignupFormSchema>,
+  ) => Promise<void>;
   confirmField?: boolean;
   link: {
     to: string;
     title: string;
   };
+  schema: typeof SigninFormSchema | typeof SignupFormSchema;
 }
-
-type FormData = z.infer<typeof FormSchema>;
 
 export const FormLayout = ({
   buttonTitle,
   onSubmit,
   confirmField,
   link,
+  schema,
 }: FormLayoutProps) => {
-  const form = useForm<FormData>({
-    resolver: zodResolver(FormSchema),
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
     mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+      ...(confirmField ? { confirmPassword: "" } : {}),
+    },
   });
 
   const {
