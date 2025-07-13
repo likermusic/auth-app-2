5365610b812bfe6917b60e61a064da65ed1bff9b
Sergey Timofeev <drumliker@yandex.ru>
2025-07-13 12:21:13 +0300

    add server verify token

--- src/entities/user/api/auth.ts
+++ src/entities/user/api/auth.ts
@@ -1,6 +1,8 @@
 import { api } from "@/shared/api/axios-instance";
 import type { IUser } from "../types";
 import { ROUTES } from "@/shared/api/constants";
+import Cookies from "js-cookie";
+// import * as Cookies from "js-cookie";
 
 interface IUserRequest extends Pick<IUser, "email" | "password"> {}
 
@@ -16,4 +18,10 @@ interface IUserResponse {
 export const authApi = {
   signin: (data: IUserRequest) => api.post<IUserResponse>(ROUTES.SIGNIN, data),
   signup: (data: IUserRequest) => api.post<IUserResponse>(ROUTES.SIGNUP, data),
+  protected: () =>
+    api.get(ROUTES.PROTECTED, {
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }),
 };
