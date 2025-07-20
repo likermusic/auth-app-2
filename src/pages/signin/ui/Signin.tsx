4504eea0dc2a2ce1b9e21f4c73a2866e08a18b99
Sergey Timofeev <drumliker@yandex.ru>
2025-07-20 12:36:40 +0300

    add Auth acheckout HOC

--- src/pages/signin/ui/Signin.tsx
+++ src/pages/signin/ui/Signin.tsx
@@ -1,10 +1,24 @@
 import { authApi } from "@/entities/user/api/auth";
-import { SigninForm } from "@/features/auth";
+import { SigninForm, withCheckAuth } from "@/features/auth";
 
 import { Button } from "@/shared/ui/button";
 import { FormPageLayout } from "@/shared/ui/layouts/FormPageLayout";
 import { AxiosError } from "axios";
 
-export const Signin = () => {
+// export const Signin = () => {
+//   return <FormPageLayout title="Sign in" form={<SigninForm />} />;
+// };
+
+// export const Signin = ({ title }: { title: any }) => {
+//   return <FormPageLayout title={title} form={<SigninForm />} />;
+// };
+
+export const Signin = withCheckAuth(() => {
   return <FormPageLayout title="Sign in" form={<SigninForm />} />;
-};
+});
+
+// const Signin = () => {
+//   return <FormPageLayout title="Sign in" form={<SigninForm />} />;
+// };
+
+// export const ProptectedSignin = withCheckAuth(SigninComp);
--- src/pages/signup/ui/Signup.tsx
+++ src/pages/signup/ui/Signup.tsx
@@ -1,8 +1,8 @@
 import { authApi } from "@/entities/user/api/auth";
-import { SignupForm } from "@/features/auth";
+import { SignupForm, withCheckAuth } from "@/features/auth";
 import { FormPageLayout } from "@/shared/ui/layouts/FormPageLayout";
 import React from "react";
 
-export const Signup = () => {
+export const Signup = withCheckAuth(() => {
   return <FormPageLayout title="Sign up" form={<SignupForm />} />;
-};
+});
