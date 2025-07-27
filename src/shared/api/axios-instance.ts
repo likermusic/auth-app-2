7230b8e104729c36fe6e1e2f0491bcc7024338da
Sergey Timofeev <drumliker@yandex.ru>
2025-07-27 12:10:16 +0300

    add httponly cookie

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -3,18 +3,19 @@ import Cookies from "js-cookie";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
-export const protectedApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
+// export const protectedApi = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
 
-protectedApi.interceptors.request.use((config) => {
-  const token = Cookies.get("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  } else {
-    throw new Error("Cancel query. Token is not found");
-  }
-  return config;
-});
+// protectedApi.interceptors.request.use((config) => {
+//   const token = Cookies.get("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   } else {
+//     throw new Error("Cancel query. Token is not found");
+//   }
+//   return config;
+// });
